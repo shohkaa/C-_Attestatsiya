c98e79a70b50544c3717fb1c542d9da4b00fbade
Baxodir <baxodirochildiyev55@gmail.com>
2024-02-16 12:50:34 +0500

    addition this file:work1

--- work1/Program.cs
+++ work1/Program.cs
@@ -30,4 +30,6 @@ int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int N = Convert.ToInt32(Console.ReadLine()); 
 
+Console.Write($"M={M}; N={N}; => ");
+
 NaturalNumbers(M, N);
